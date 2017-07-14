550654f97aa4f628d510c6f5615bbcfb0659bdfa
Keita Kawaji <keita.kawaji@human.ait.kyushu-u.ac.jp>
2017-07-14 17:33:25 +0900

    first commit

---
+++ homework/DP.py
@@ -0,0 +1,36 @@
+import time
+
+def DP():
+    # N = int(input('n: '))
+    # W = int(input('W: '))
+    # w = list(map(int, input('w: ').split()))
+    # v = list(map(int, input('v: ').split()))
+
+    w = [2,2,1,3,4,2,5,7,7,5,3,3,1,7,5,9,4,9]
+    v = [4,5,2,8,3,5,6,7,3,6,2,6,7,6,1,9,6,5]
+    N = len(w)
+    W = 30
+
+    print('input------')
+    print(N)
+    print(W)
+    print(w)
+    print(v)
+    print('-----------')
+
+    dp = [[0 for i in range(W + 1)] for j in range(N + 1)]
+
+    for j in range(N) :
+        for i in range(N - 1, -1, -1) :
+            for j in range(W + 1) :
+                if j < w[i] :
+                    dp[i][j] = dp[i+1][j]
+                else :
+                    dp[i][j] = max(dp[i+1][j], dp[i+1][j-w[i]]+v[i])
+    print('Ans, ' + str(dp[0][W]))
+
+if __name__ == '__main__':
+    start = time.time()
+    DP()
+    elapsed_time = time.time() - start
+    print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
